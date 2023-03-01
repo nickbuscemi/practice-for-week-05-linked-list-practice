d388ee653a20bcca34bc23398d0e8e0d285a739c
Nick <nick.buscemi13@gmail.com>
2023-03-01 14:19:11 -0500

    finished-phase-1

--- linked-list.js
+++ linked-list.js
@@ -7,15 +7,33 @@ class LinkedListNode {
 
 class LinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    const newNode = new LinkedListNode(val, null);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
+    const newNode = new LinkedListNode(val, null);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.length++;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+    this.length++;
   }
 
   // You can use this function to help debug
